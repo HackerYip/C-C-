622386d275ce8feeac6e59d564898b3b88a7ad40
HakerYip <975231661@qq.com>
2021-11-08 22:20:10 +0800

    11月8日代码

---
+++ test_11_07/test_11_07/game.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#define ROW 3
+#define COL 3
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+void InitBoard(char board[ROW][COL], int row, int col);//初始化棋盘
+
+void DisplayBoard(char board[ROW][COL], int row, int col);//打印棋盘
+
+void player_move(char board[ROW][COL], int row, int col);//玩家下棋
+
+void computer_move(char board[ROW][COL], int row, int col);//电脑下棋
+
+//判断输赢的代码
+//玩家赢 --- '*'
+//电脑赢--- '#'
+//平局--- 'Q'
+//继续--- 'C'
+
+
+char is_win(char board[ROW][COL], int row, int col);
