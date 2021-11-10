9194ffc10374638f24e37c1613ddbe2932684abb
HakerYip <975231661@qq.com>
2021-11-10 21:30:21 +0800

    11月10号代码

---
+++ test_11_10/test_11_10/game.h
@@ -0,0 +1,25 @@
+#pragma once
+
+ #include<stdio.h>
+ #include<time.h>
+ #include<stdlib.h>
+
+#define ROW 9
+#define COL 9
+
+#define ROWS ROW+2
+#define COLS COL+2
+
+#define EASY_COUNT 10
+
+//≥ı ºªØ∆Â≈Ã
+void InitBoard(char board[ROWS][COLS], int rows, int cols, char ret);
+
+//¥Ú”°∆Â≈Ã
+void DisplayBoard(char board[ROWS][COLS], int row, int col);
+
+//≤º÷√¿◊
+void Setmine(char mine[ROWS][COLS], int row, int col);
+
+//≈≈≤È¿◊
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
