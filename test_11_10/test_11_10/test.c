cb276deea3b18c146aaec60613a535f82f923fdb
HakerYip <975231661@qq.com>
2021-11-10 21:30:10 +0800

    11月10号代码

---
+++ test_11_10/test_11_10/test.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include"game.h"
+
+void menu()
+{
+    printf("***********************************\n");
+    printf("************  1.play  *************\n");
+    printf("************  0.exit  *************\n");
+    printf("***********************************\n");
+}
+
+
+void game()
+{
+    //创建数组
+    char mine[ROWS][COLS] = { 0 };//布置好的雷的信息
+    char show[ROWS][COLS] = { 0 };//存放排查出的雷的信息
+
+    //初始化mine数组为全'0'
+    InitBoard(mine, ROWS, COLS, '0');
+    //初始化show数组为全'*'
+    InitBoard(show, ROWS, COLS, '*');
+    
+    //打印棋盘
+    //DisplayBoard(mine, ROW, COL);
+    
+    //布置雷
+    Setmine(mine, ROW, COL);
+    DisplayBoard(show, ROW, COL);
+
+    //DisplayBoard(mine, ROW, COL);
+    //排雷
+    FindMine(mine, show, ROW, COL);
+}
+
+
+void test()
+{
+    int input = 0;
+    srand((unsigned int)time(NULL));
+    do
+    {
+        menu();
+        printf("请选择:>");
+        scanf("%d", &input);
+        switch (input)
+        {
+        case 1:
+            game();
+            break;
+        case 0:
+            printf("退出游戏\n");
+            break;
+        default:
+            printf("选择错误，请重新输入\n");
+            break;
+        }
+    } while (input);
+}
+
+
+int main()
+{
+    test();
+    return 0;
+}
