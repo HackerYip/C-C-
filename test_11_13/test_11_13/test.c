8d6771d0d47df3dd94d870eb8ca17cd5abed2e10
HakerYip <975231661@qq.com>
2021-11-13 23:42:38 +0800

    11月13日代码，仍需补充

---
+++ test_11_13/test_11_13/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+        int a = 8;
+        int b = 6;
+        printf("交换前: a=%d, b=%d\n", a, b);
+        a = a ^ b;
+        b = a ^ b;//a ^ b^b
+        a = a ^ b;//a ^ b ^ a
+
+        printf("交换后: a=%d, b=%d\n", a, b);
+    return 0;
+}
