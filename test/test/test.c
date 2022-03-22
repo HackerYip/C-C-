4cb5d68a215009eb755f9f4aad7c01f2c6c6b7f1
HakerYip <975231661@qq.com>
2022-03-23 00:00:32 +0800

    C语言复习第二天

---
+++ test/test/test.c
@@ -0,0 +1,92 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//void Swap(int *px, int*py)
+//{
+//    int tmp = 0;
+//
+//    tmp = *px;
+//    *px = *py;
+//    *py = tmp;
+//}
+//
+//int main()
+//{
+//int a = 0;
+//int b = 0;
+//int c = 0;
+//
+//
+//scanf("%d %d %d", &a, &b, &c);
+//
+//    if(a < b)
+//    {
+//        Swap(&a, &b);
+//    }
+//
+//    if(a < c)
+//    {
+//        Swap(&a, &c);
+//    }
+//
+//    if(b < c)
+//    {
+//        Swap(&b, &c);
+//    }
+//    printf("%d %d %d\n",a, b, c);
+//
+//    return 0;
+//}
+
+//int main()
+//{
+//    int i = 1;
+//    while(i <= 100)
+//    {
+//        if(i % 3 == 0)
+//        printf("%d ",i);
+//
+//        i++;
+//    }
+//    return 0;
+//}
+
+
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    int c = 0;
+//    
+//    scanf("%d %d",&a, &b);
+//
+//    while(c = a % b)
+//    {
+//        a = b;
+//        b = c;
+//    }   
+//    printf("%d\n", b);
+//
+//    return 0;
+//}
+
+
+
+//int main()
+//{
+//    int i = 1000;
+//    for(i=1000; i<=2000; i++)
+//    {
+//        if(i % 4 == 0 && i % 100 != 0)
+//        {
+//            printf("%d ",i);
+//        }
+//        
+//        if(i % 400 == 0)
+//        {
+//            printf("%d ", i);
+//        }
+//    }
+//    return 0;
+//}
